3259e7697efd5a56aa6d190efed3b4fd30cfc7c0
alex <alexhuerta147@gmail.com>
2017-10-09 15:01:25 -0500

    muestra caracteristicas del examen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,17 +8,31 @@ import {ServiceProvider} from '../../providers/service/service';
 export  class TestPendiente {
   id: number;
   name: string;
+  subject:string;
+  start_datetime:string;
+  end_datetime:string;
+  number_questions:number;
+  correct_points:number;
+  error_points:number;
+  attempts_allowed:number; 
 
-  constructor(id: number, name: string) {
+  constructor(id: number, name: string, subject: string, start_datetime: string, end_datetime: string, number_questions: number, correct_points: number, error_points: number,attempts_allowed: number) {
     this.id = id;
     this.name = name;
+    this.subject = subject;
+    this.start_datetime = start_datetime;
+    this.end_datetime = end_datetime;
+    this.number_questions = number_questions;
+    this.correct_points = correct_points;
+    this.error_points = error_points;
+    this.attempts_allowed = attempts_allowed;
   }
 }
 
 @IonicPage() 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 
 export class HomePage {
@@ -27,16 +41,25 @@ export class HomePage {
   users: any[];
   loader: any;
   type: string;
+  type2: string;
   data: string;
   examen : TestPendiente[]=[];
 
-  
+  nombreExamen:string="";
+  number_questions:number=0;
+  correct_points:number=0;
+  error_points:number=0;
+  start_datetime:string="";
+  end_datetime:string="";
+
+
   constructor(public navCtrl: NavController, 
               private navParams: NavParams,
               public service: ServiceProvider,
               public loadingCtrl: LoadingController) {
    
     this.type = "pendiente";
+    this.type2 = "inicio";
     this.data = this.navParams.get('data');//datos token pasados a la vista home
     this.examenPendiente();//muestra los examenes pendientes
     //  this.slides = [
@@ -100,16 +123,14 @@ export class HomePage {
 
 //extrayendo la data de examenes
   examenPendiente() {
+    this.examen = [];
     //this.presentLoading();
     this.service.getDataExamenLocal(this.data["token"]).subscribe(
       data => {
-        /*this.examen = data;
-        console.log(data);
-        this.loader.dismiss();*/
         let tests = data["data"];
-        console.log(tests[0]["id_test"]);
+        //console.log(tests[0]["id_test"]);
         for(var i=0;i<tests.length;i++){
-          this.examen.push(new TestPendiente(tests[i]["id_test"],tests[i]["name"])); 
+          this.examen.push(new TestPendiente(tests[i]["id_test"],tests[i]["name"],tests[i]["subject"],tests[i]["start_datetime"],tests[i]["end_datetime"],tests[i]["number_questions"],tests[i]["correct_points"],tests[i]["error_points"],tests[i]["attempts_allowed"])); 
         }
       },
       err => {
@@ -161,10 +182,45 @@ export class HomePage {
 //           () => console.log('Movie Search Complete')
 //       );
 //   }
+
 //pasar a la hoja de inicio del examen
-  ViewIExamen() {
-    this.navCtrl.push(InicioexamenPage);
+  ViewIExamen(exam) {
+    //this.navCtrl.push(InicioexamenPage);
+    this.type2="examenPendiente";
+    this.type="";
+    this.nombreExamen = exam.name;
+    this.number_questions = exam.number_questions;
+    this.correct_points = exam.correct_points;
+    this.error_points = exam.error_points;
+    this.start_datetime = exam.start_datetime;
+    this.end_datetime = exam.end_datetime;
+
   }
+
+  cambio(horadia){
+    let format = new Date(horadia);
+    return this.addo(format.getUTCHours())+":"+this.addo(format.getUTCMinutes())+":"+this.addo(format.getUTCSeconds());
+  }
+  getFecha(horadia){
+    let format = new Date(horadia);
+    return this.addo(format.getUTCFullYear())+"-"+this.addo(format.getUTCMonth())+"-"+this.addo(format.getUTCDay());
+  }
+
+  addo(comp){
+    return (((comp+"").length==1)?"0"+comp:comp);
+  }
+
+  back(){
+    this.type2="inicio";
+    this.type="pendiente";
+    this.nombreExamen = "";
+    this.number_questions = 0;
+    this.correct_points = 0;
+    this.error_points = 0;
+    this.start_datetime = "";
+    this.end_datetime = "";
+  }
+
 
 //     View(member)
 //     {
